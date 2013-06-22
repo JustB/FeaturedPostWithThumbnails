d55cca58c37fd278b7cf3924b1f1d9e9aeb95d85
Giustino Borzacchiello <giustinob@gmail.com>
2013-06-22 17:01:21 +0200

    Moved metabox declarations in their own file

--- index.php
+++ index.php
@@ -146,8 +146,6 @@ function featured_posts_YIW($args = null) {
 	 * properly
 	 * Info: http://codex.wordpress.org/Template_Tags/get_posts
 	 *
-	 * @todo Aggiungere esempio per mostrare/escludere categorie e pagine in
-	 * questo commento
 	 */
 	global $post;
 	if ( (strcmp($show, 'category') == 0 ) && ($cat_ID)) {
@@ -165,57 +163,7 @@ function featured_posts_YIW($args = null) {
 
 /* END featured_posts_YIW */
 
-/**
- * Aggiunge/rimuove il campo personalizzato featured
- * Add/remove featured custom field
- *
- * @param integer $post_ID
- */
-function YIW_add_featured($post_ID) {
-	$articolo = get_post($post_ID);
-	if ( isset($_POST['insert_featured_post']) ) {
-		if ( $_POST['insert_featured_post'] == 'yes' ) {
-			add_post_meta($articolo->ID, 'featured', 1, TRUE) ||
-					  update_post_meta($articolo->ID, 'featured', 1);
-		} elseif ( $_POST['insert_featured_post'] == 'no' ) {
-			delete_post_meta($articolo->ID, 'featured');
-		}
-	}
-}
 
-/**
- *
- * Mostra il form featured nella sezione "Scrivi Post"
- * Shows featured form in "Write Post" section
- */
-function YIW_post_box() {
-	global $post;
-	$yes = '';
-	$no = '';
-	$featured = get_post_meta($post->ID, 'featured', 1);
-	if ( $featured ) {
-		$yes = 'selected="selected"';
-	} else {
-		$no = 'selected="selected"';
-	}
-	echo '<label for="insert_featured_post">' .
-	__('Featured post?', YIW_TEXT_DOMAIN) . '</label>';
-	echo '<select name="insert_featured_post" id="insert_featured_post">';
-	echo '<option value="yes" ' . $yes . ' >' .
-	__('Yes', YIW_TEXT_DOMAIN) . '</option>';
-	echo '<option value="no" ' . $no . ' >' .
-	__('No ', YIW_TEXT_DOMAIN) . '</option>';
-	echo '</select>';
-}
-
-function my_post_options_box() {
-	add_meta_box('post_info', __('Featured', YIW_TEXT_DOMAIN),
-			  'YIW_post_box', 'post', 'side', 'high');
-}
-
-add_action('admin_menu', 'my_post_options_box');
-add_action('new_to_publish', 'YIW_add_featured');
-add_action('save_post', 'YIW_add_featured');
 
 
 /*
