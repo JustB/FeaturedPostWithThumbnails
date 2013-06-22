4490e5e11d44b9340d43f2262ccba152947af1e1
Giustino Borzacchiello <giustinob@gmail.com>
2013-06-22 17:40:49 +0200

    Moved custom column in separate file

--- featured-posts-with-thumbnail.php
+++ featured-posts-with-thumbnail.php
@@ -167,31 +167,7 @@ function featured_posts_YIW($args = null) {
 
 
 
-/*
- * Add Featured column in Admin post list
- */
-add_filter('manage_posts_columns', 'yiw_add_column');
 
-function yiw_add_column($defaults) {
-	$defaults['yiw-featured'] = __('Featured', YIW_TEXT_DOMAIN);
-	return $defaults;
-}
-
-/*
- * Recupera dal database tutti i post che hanno il custom field featured
- * attivato
- */
-add_action('manage_posts_custom_column', 'yiw_featured_column', 10, 2);
-
-function yiw_featured_column($column_name, $id) {
-	if ( $column_name == 'yiw-featured' ) {
-        if(get_post_meta($id, '_yiw_featured_post', true)) {
-            _e("Yes", YIW_TEXT_DOMAIN);
-        } else {
-            _e("No", YIW_TEXT_DOMAIN);
-        }
-	}
-}
 
 function yiw_add_widget_script(){
 	global $featured_post_plugin_path;
---
+++ includes/admin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Add Featured column in Admin post list
+ */
+add_filter('manage_posts_columns', 'yiw_add_column');
+
+function yiw_add_column($defaults) {
+    $defaults['yiw-featured'] = __('Featured', YIW_TEXT_DOMAIN);
+    return $defaults;
+}
+
+/*
+ * Recupera dal database tutti i post che hanno il custom field featured
+ * attivato
+ */
+add_action('manage_posts_custom_column', 'yiw_featured_column', 10, 2);
+
+function yiw_featured_column($column_name, $id) {
+    if ( $column_name == 'yiw-featured' ) {
+        if(get_post_meta($id, '_yiw_featured_post', true)) {
+            _e("Yes", YIW_TEXT_DOMAIN);
+        } else {
+            _e("No", YIW_TEXT_DOMAIN);
+        }
+    }
+}
