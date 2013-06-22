9b451034e4ccd2e0986b810bd701409e51bbe2b4
Giustino Borzacchiello <giustinob@gmail.com>
2013-06-22 15:41:34 +0200

    Reformatted widget form

--- views/admin.php
+++ views/admin.php
@@ -1,69 +1,94 @@
 <?php
-echo '<p>';
-echo '<label for="' . $this->get_field_id('title') . '">' .
-    _e('Title:', YIW_TEXT_DOMAIN) . '</label>';
-echo '<input id="' . $this->get_field_id('title') . '" name="' .
-    $this->get_field_name('title') . '" value="' . $instance['title'] .
-    '" style="width:100%;" class="widefat" />';
-echo '</p>';
+/**
+ * @var $this Featured_posts
+ * @var $instance Featured_posts
+ */
+?>
+<p>
+    <label for="<?php echo $this->get_field_id('title'); ?>">
+        <?php _e('Title:', YIW_TEXT_DOMAIN); ?>
+    </label>
+    <input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>"
+           value="<?php echo esc_attr($instance['title']); ?>"
+           style="width:100%;" class="widefat"/>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('showposts') ?>">
+        <?php _e('How many posts do you want to display?', YIW_TEXT_DOMAIN); ?>
+    </label>
+    <select name="<?php echo $this->get_field_name('showposts'); ?>"
+            id="<?php echo $this->get_field_id('showposts') ?>">
+        <?php for ($i = 0; $i <= 100; $i++) : ?>
+        <option class="level-0"
+                value="<?php echo $i ?>" <?php selected($instance['showposts'], $i) ?>><?php echo $i ?></option>
+        <?php endfor; ?>
+    </select>
+</p>
 
-echo '<p>';
-echo '<label for="' . $this->get_field_id('showposts') . '">' .
-    _e('How many posts do you want to display?', YIW_TEXT_DOMAIN) . '</label>';
-echo '<select name="' . $this->get_field_name('showposts') .
-    '" id="' . $this->get_field_id('showposts') . '" >';
-for ($i = 0; $i <= 100; $i++) {
-    echo '<option class="level-0" value="' . $i . '"' . selected($instance['showposts'], $i) . '>' . $i . '</option>';
-}
-echo '</select>';
-echo '</p>';
-echo '<p>';
-echo '<label for="' . $this->get_field_id('orderby') . '">' .
-    __('Choose type of order:', YIW_TEXT_DOMAIN) . '</label>';
-echo '<select name="' . $this->get_field_name('orderby') . '" id="' .
-    $this->get_field_id('orderby') . '" >';
-echo '<option class="level-0" value="rand" ' . selected($instance['orderby'], 'random') . ' >' .
-    __('Random', YIW_TEXT_DOMAIN) . '</option>';
-echo '<option class="level-0" value="title" ' . selected($instance['orderby'], 'title') . ' >' .
-    __('Title', YIW_TEXT_DOMAIN) . '</option>';
-echo '<option class="level-0" value="date" ' . selected($instance['orderby'], 'date') . ' >' .
-    __('Date', YIW_TEXT_DOMAIN) . '</option>';
-echo '<option class="level-0" value="author" ' . selected($instance['orderby'], 'author') . ' >' .
-    __('Author', YIW_TEXT_DOMAIN) . '</option>';
-echo '<option class="level-0" value="modified" ' . selected($instance['orderby'], 'modified') . ' >' .
-    __('Modified', YIW_TEXT_DOMAIN) . '</option>';
-echo '<option class="level-0" value="ID" ' . selected($instance['orderby'], 'ID') . ' >' .
-    __('ID', YIW_TEXT_DOMAIN) . '</option>';
-echo '</select>';
-echo '</p>';
-echo '<p>';
-echo '<label for="' . $this->get_field_id('width-thumb') . '">' .
-    __('Width Thumbnail', YIW_TEXT_DOMAIN) . '</label>';
-echo '<input id="' . $this->get_field_id('width-thumb') .
-    '" name="' . $this->get_field_name('width-thumb') . '" value="' .
-    $instance['width-thumb'] . '" style="width:20%;" class="widefat" />';
-echo '</p>';
-echo '<p>';
-echo '<label for="' . $this->get_field_id('height-thumb') . '">' .
-    __('Height Thumbnail', YIW_TEXT_DOMAIN) . '</label>';
-echo '<input id="' . $this->get_field_id('height-thumb') .
-    '" name="' . $this->get_field_name('height-thumb') . '" value="' .
-    $instance['height-thumb'] . '" style="width:20%;" class="widefat" />';
-echo '</p>';
-echo '<p id="yiw_featured_post_show">';
-echo '<label for="' . $this->get_field_id('show') . '">' . _e("Featured or category?", YIW_TEXT_DOMAIN) . '</label>';
-echo '<select id="' . $this->get_field_id('show') . '" name="' . $this->get_field_name('show') . '">';
-echo '<option value="featured" ' . selected($instance['show'], 'featured') . '>Featured</option>';
-echo '<option value="category" ' . selected($instance['show'], 'category') . '>Category</option>';
-echo '</select>';
-echo '</p>';
 
-echo '<p id="yiw_featured_post_category">';
-echo '<label for="' . $this->get_field_id('category') . '">' . _e('Category', YIW_TEXT_DOMAIN) . '</label>';
-echo '<select id="' . $this->get_field_id('category') . '" name="' . $this->get_field_name('category') . '">';
-$categories = get_categories();
-foreach ($categories as $c) {
-    echo '<option value="'. $c->cat_ID .'" ' . selected($instance['category'], $c->cat_ID) . '>'. $c->name .'</option>';
-}
-echo '</select>';
-echo '</p>';
+<p>
+    <label for="<?php $this->get_field_id('orderby') ?>">
+        <?php _e('Choose type of order:', YIW_TEXT_DOMAIN) ?>
+    </label>
+    <select name="<?php $this->get_field_name('orderby') ?>" id="<?php echo $this->get_field_id('orderby') ?>">
+        <option class="level-0" value="rand" <?php selected($instance['orderby'], 'random') ?>>
+            <?php _e('Random', YIW_TEXT_DOMAIN) ?>
+        </option>
+        <option class="level-0" value="title" <?php selected($instance['orderby'], 'title') ?>>
+            <?php _e('Title', YIW_TEXT_DOMAIN) ?>
+        </option>
+        <option class="level-0" value="date" <?php selected($instance['orderby'], 'date') ?>>
+            <?php _e('Date', YIW_TEXT_DOMAIN) ?>
+        </option>
+        <option class="level-0" value="author" <?php selected($instance['orderby'], 'author') ?>>
+            <?php _e('Author', YIW_TEXT_DOMAIN) ?>
+        </option>
+        <option class="level-0" value="modified" <?php selected($instance['orderby'], 'modified') ?>>
+            <?php _e('Modified', YIW_TEXT_DOMAIN) ?>
+        </option>
+        <option class="level-0" value="ID" <?php selected($instance['orderby'], 'ID') ?>>
+            <?php _e('ID', YIW_TEXT_DOMAIN) ?>
+        </option>
+    </select>
+</p>
+
+<p>
+    <label for="<?php echo $this->get_field_id('width-thumb') ?>">
+        <?php _e('Width Thumbnail', YIW_TEXT_DOMAIN); ?>
+    </label>
+    <input id="<?php echo $this->get_field_id('width-thumb') ?>"
+           name="<?php echo $this->get_field_name('width-thumb') ?>"
+           value="<?php echo esc_attr($instance['width-thumb']); ?>" style="width:20%;" class="widefat"/>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('height-thumb') ?>">
+        <?php _e('Height Thumbnail', YIW_TEXT_DOMAIN) ?>
+    </label>
+    <input id="<?php echo $this->get_field_id('height-thumb') ?>"
+           name="<?php echo $this->get_field_name('height-thumb') ?>"
+           value="<?php echo esc_attr($instance['height-thumb']) ?>" style="width:20%;" class="widefat"/>
+</p>
+
+<p id="yiw_featured_post_show">
+    <label for="<?php $this->get_field_id('show') ?>"><?php _e("Featured or category?", YIW_TEXT_DOMAIN) ?></label>
+    <select id="<?php echo $this->get_field_id('show') ?>" name="<?php echo $this->get_field_name('show') ?>">
+        <option value="featured" <?php selected($instance['show'], 'featured') ?>>
+            <?php _e('Featured', YIW_TEXT_DOMAIN) ?>
+        </option>
+        <option value="category" <?php selected($instance['show'], 'category') ?>>
+            <?php _e('Category', YIW_TEXT_DOMAIN) ?>
+        </option>
+    </select>
+</p>
+<p id="yiw_featured_post_category">
+    <label for="<?php echo $this->get_field_id('category') ?>"><?php _e('Category', YIW_TEXT_DOMAIN) ?></label>
+    <select id="<?php $this->get_field_id('category') ?>" name="<?php $this->get_field_name('category') ?>">
+        <?php
+        $categories = get_categories();
+        foreach ($categories as $c): ?>
+            <option value="<?php echo $c->cat_ID ?>" <?php selected($instance['category'], $c->cat_ID) ?>>
+                <?php echo $c->name ?>
+            </option>
+            <?php endforeach; ?>
+    </select>
+</p>
